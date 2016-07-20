e152c6c92d4732c82f410fa6972c23da5b740350
Paolo Bosetti <paolo.bosetti@unitn.it>
2016-07-20 09:41:35 +0200

    Methods for parsing MarvelMind packets. Still to check against coorect byte order against the real hardware

--- lib/mm_gps/mm_gps_module.rb
+++ lib/mm_gps/mm_gps_module.rb
@@ -1,5 +1,29 @@
+class GPSException < Exception; end
+
 module MmGps
   def self.valid_crc16?(str)
     crc16(str) == 0
+  end
+  
+  def self.parse_packet(buf)
+    raise GPSException, "Invalid CRC" unless valid_crc16?(buf)
+    header = buf[0...5].unpack('CCS<C')
+    
+    if header[2] == 256 then # Regular GPS Data
+      result = {}
+      payload = buf[5...16].unpack('L<S<3C')
+      result = %I(ts x y z f).zip(payload).to_h
+    elsif header[2] == 512 then # Frozen
+      len = buf[5].unpack('C')[0]
+      result = []
+      len.times do |i|
+        offset = 6 + i * 8
+        payload = buf[offset...(offset + 8)].unpack('CS<3C')
+        result << %I(address x y z reserved).zip(payload).to_h
+      end
+    else
+      raise GPSException, "Unexpected packet type #{header[2]}"
+    end
+    return result
   end
 end
